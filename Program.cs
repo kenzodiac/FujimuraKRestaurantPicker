d5a9a42e8bcee41887ad0d92cbc58ae432a6612b
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 14:59:25 -0700

    Finish

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //Date Revised: 10-20-2022
 //GitHub Challenges #9: Restaurant Picker
 //This is a console program that helps a user select a restaurant to eat from. The user selects a cateogry of restaurant, and then the program chooses at random which restaurant to pick from a list of predetermined choices, discriminating based on the preselected category.
-//Peer Reviewed By: 
+//Peer Reviewed By:  Harrison Busby: "Ken's program is excellent. You can really tell that its extremely organized, and very easy to read. It shows his incredible attention to detail. I really liked how when he used his string arrays to list his restaurants, that he made them super easy to be able to ready. Overall, excellent code and excellent work. Good job Ken! :)
 
 Console.Clear();
 //declare variables
